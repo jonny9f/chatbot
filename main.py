62afc437d3ca33ffd477c45278738a31f635a1c2
jonw <jon.wadelton@gmail.com>
2023-11-16 11:08:11 +0000

    Add history

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta",
 
 tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-beta",
                                           torch_dtype="auto")
-def stream(user_prompt):
+
+def stream(user_prompt, history=[]):
     runtimeFlag = "cuda:0"
     system_prompt = 'You are Gathnex AI, an intelligent assistant. You are rude and have a bad attitude.'
     E_INST = "</s>"
-    user, assistant = "<|user|>", "<|assistant|>"
 
-    prompt = f"{system_prompt}{E_INST}\n{user}\n{user_prompt.strip()}{E_INST}\n{assistant}\n"
+    user, assistant = "<|user|>", "<|assistant|>"
+    
+    # Combine the previous history with the current user prompt
+    combined_history = history + [f"{user}\n{user_prompt.strip()}{E_INST}\n{assistant}"]
+    prompt = f"{system_prompt}{E_INST}\n" + "".join(combined_history)
 
     inputs = tokenizer([prompt], return_tensors="pt").to(runtimeFlag)
 
@@ -29,12 +33,14 @@ def stream(user_prompt):
     output = model.generate(**inputs, streamer=streamer, max_new_tokens=500)
 
     response = tokenizer.decode(output[0], skip_special_tokens=True)
+    
+    # Update the history with the latest exchange
+    history.append(f"{user}\n{user_prompt.strip()}{E_INST}\n{assistant}\n{response}")
 
-    return response
+    return response, history
 
+history = []
 while True:
     user_input = input("You: ")
-    response = stream(user_input)
+    response, history = stream(user_input, history)
     print("Bot:", response)
-
-
